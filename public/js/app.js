23d80b169cff114ab41cd0b88406fa63a7e682e7
Patrick Maciel <patrickmaciel.info@gmail.com>
2018-09-10 08:23:46 -0300

    Criando o componente Formulário

--- public/js/app.js
+++ public/js/app.js
@@ -13911,6 +13911,7 @@ Vue.component('tabela-lista', __webpack_require__(66));
 Vue.component('migalhas', __webpack_require__(71));
 Vue.component('modal', __webpack_require__(76));
 Vue.component('modal-link', __webpack_require__(81));
+Vue.component('formulario', __webpack_require__(86));
 
 var app = new Vue({
   el: '#app'
@@ -49263,6 +49264,158 @@ if (false) {
   module.hot.accept()
   if (module.hot.data) {
     require("vue-hot-reload-api")      .rerender("data-v-30f6454a", module.exports)
+  }
+}
+
+/***/ }),
+/* 86 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var disposed = false
+function injectStyle (ssrContext) {
+  if (disposed) return
+  __webpack_require__(87)
+}
+var normalizeComponent = __webpack_require__(40)
+/* script */
+var __vue_script__ = __webpack_require__(89)
+/* template */
+var __vue_template__ = __webpack_require__(90)
+/* template functional */
+var __vue_template_functional__ = false
+/* styles */
+var __vue_styles__ = injectStyle
+/* scopeId */
+var __vue_scopeId__ = null
+/* moduleIdentifier (server only) */
+var __vue_module_identifier__ = null
+var Component = normalizeComponent(
+  __vue_script__,
+  __vue_template__,
+  __vue_template_functional__,
+  __vue_styles__,
+  __vue_scopeId__,
+  __vue_module_identifier__
+)
+Component.options.__file = "resources/js/components/Formulario.vue"
+
+/* hot reload */
+if (false) {(function () {
+  var hotAPI = require("vue-hot-reload-api")
+  hotAPI.install(require("vue"), false)
+  if (!hotAPI.compatible) return
+  module.hot.accept()
+  if (!module.hot.data) {
+    hotAPI.createRecord("data-v-c3f6ecc6", Component.options)
+  } else {
+    hotAPI.reload("data-v-c3f6ecc6", Component.options)
+  }
+  module.hot.dispose(function (data) {
+    disposed = true
+  })
+})()}
+
+module.exports = Component.exports
+
+
+/***/ }),
+/* 87 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// style-loader: Adds some css to the DOM by adding a <style> tag
+
+// load the styles
+var content = __webpack_require__(88);
+if(typeof content === 'string') content = [[module.i, content, '']];
+if(content.locals) module.exports = content.locals;
+// add the styles to the DOM
+var update = __webpack_require__(56)("d125fe66", content, false, {});
+// Hot Module Replacement
+if(false) {
+ // When the styles change, update the <style> tags
+ if(!content.locals) {
+   module.hot.accept("!!../../../node_modules/css-loader/index.js!../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-c3f6ecc6\",\"scoped\":false,\"hasInlineConfig\":true}!../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./Formulario.vue", function() {
+     var newContent = require("!!../../../node_modules/css-loader/index.js!../../../node_modules/vue-loader/lib/style-compiler/index.js?{\"vue\":true,\"id\":\"data-v-c3f6ecc6\",\"scoped\":false,\"hasInlineConfig\":true}!../../../node_modules/vue-loader/lib/selector.js?type=styles&index=0!./Formulario.vue");
+     if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+     update(newContent);
+   });
+ }
+ // When the module is disposed, remove the <style> tags
+ module.hot.dispose(function() { update(); });
+}
+
+/***/ }),
+/* 88 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(45)(false);
+// imports
+
+
+// module
+exports.push([module.i, "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", ""]);
+
+// exports
+
+
+/***/ }),
+/* 89 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ __webpack_exports__["default"] = ({
+    props: ['action', 'class', 'method', 'encytpe', 'style', 'token']
+});
+
+/***/ }),
+/* 90 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "form",
+    {
+      class: _vm.classe || "form",
+      style: _vm.style,
+      attrs: {
+        action: _vm.action,
+        method: _vm.method,
+        enctype: _vm.encytpe || "multipart/form-data"
+      }
+    },
+    [
+      _vm.method != "post" && _vm.method != "get"
+        ? _c("input", { attrs: { type: "hidden", name: "_token", value: "" } })
+        : _vm._e(),
+      _vm._v(" "),
+      _c("input", { attrs: { type: "hidden", name: "_method", value: "" } }),
+      _vm._v(" "),
+      _vm._t("default")
+    ],
+    2
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+module.exports = { render: render, staticRenderFns: staticRenderFns }
+if (false) {
+  module.hot.accept()
+  if (module.hot.data) {
+    require("vue-hot-reload-api")      .rerender("data-v-c3f6ecc6", module.exports)
   }
 }
 
---
+++ resources/js/components/Formulario.vue
@@ -0,0 +1,25 @@
+<template>
+    <form :action="action" :class="classe || 'form'" :method="method" :enctype="encytpe || 'multipart/form-data'" :style="style">
+        <input v-if="method != 'post' && method != 'get'" type="hidden" name="_token" value="">
+        <input type="hidden" name="_method" value="">
+
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+    export default {
+        props: [
+            'action',
+            'class',
+            'method',
+            'encytpe',
+            'style',
+            'token'
+        ]
+    }
+</script>
+
+<style>
+
+</style>
