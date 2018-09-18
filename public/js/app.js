3189e42205273b051030d7aa405b54d6fda7d3bc
Patrick Maciel <patrickmaciel.info@gmail.com>
2018-09-18 07:44:17 -0300

    Corrigido problema no refresh da aplicação

--- public/js/app.js
+++ public/js/app.js
@@ -14351,7 +14351,10 @@ Vue.component('formulario', __webpack_require__(80));
 
 var app = new Vue({
     el: '#app',
-    store: store
+    store: store,
+    mounted: function mounted() {
+        document.getElementById('app').style.display = 'block';
+    }
 });
 
 /***/ }),
